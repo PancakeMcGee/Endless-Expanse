6a6036dbbdc1db8ab591b5d07d24ed01a04bc02b
PancakeMcGee <94121786+PancakeMcGee@users.noreply.github.com>
2022-05-28 20:39:21 -0400

    5/28/2022

---
+++ script.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+var character = document.getElementById("character");
+document.addEventListener("click",jump);
+function jump(){
+    if(character.classList == "animate"){return;}
+    character.classList.add("animate");
+    setTimeout(removeJump, 300);
+}
+function removeJump(){
+    character.classList.remove("animate");
+}
+
+
+
