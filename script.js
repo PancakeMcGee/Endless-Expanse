167b7a9c27c69e6c1cdbabdd370b59587944d005
PancakeMcGee <94121786+PancakeMcGee@users.noreply.github.com>
2022-05-28 22:17:18 -0400

    5/28/2022

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 var character = document.getElementById("character");
+var block = document.getElementById("block");
 document.addEventListener("click",jump);
 function jump(){
     if(character.classList == "animate"){return;}
@@ -9,6 +10,14 @@ function jump(){
 function removeJump(){
     character.classList.remove("animate");
 }
+function checkDead() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft > -20 && blockLeft < 20 && characterTop >= 130) {
+        alert("Game over!");
+    }
+}
 
+setInterval(checkDead, 10);
 
 
